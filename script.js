32cab0a5fe5789924b200d4820441e1be65449e4
swastikshelar <102980626+swastikshelar@users.noreply.github.com>
2023-03-21 12:33:20 +0530

    Weather App

---
+++ script.js
@@ -0,0 +1,38 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '20dc16c0bbmsh03fc36edab9e0f2p1a1c43jsn133f876571a0',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+const getWeather = (city)=>{
+	cityName.innerHTML = city
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+	.then(response => response.json())
+	.then((response) => {
+		
+		console.log(response)
+	   
+		temp.innerHTML = response.temp
+		// cloud_pct.innerHTML = response.cloud_pct
+		feels_like.innerHTML = response.feels_like
+		humidity.innerHTML = response.humidity
+		min_temp.innerHTML = response.min_temp
+		max_temp.innerHTML = response.max_temp
+		wind_speed.innerHTML = response.wind_speed
+		wind_degrees.innerHTML = response.wind_degrees
+		sunrise.innerHTML = response.sunrise
+		sunset.innerHTML = response.sunset	  
+	})
+	.catch(err => console.error(err));
+
+}
+ 
+submit.addEventListener("click", (e)=>{
+	e.preventDefault()
+     getWeather(city.value)
+
+})
+
+getWeather(" Navi Mumbai")
